e2247fe57aed82d3ffe07f30f6d4cb5cd84ac4e9
Shawn <FML927@live.com>
2019-04-01 00:13:59 +0800

    add more io control

--- Raspberry/2_led_ctrl_io23.py
+++ Raspberry/2_led_ctrl_io23.py
@@ -1,12 +1,18 @@
 import  RPi.GPIO as GPIO
 import time 
 
+'''
+hardware connect: GND->PIN7, Signal->PIN8, ref "_IO_" file
+'''
+
+print('setting')
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 led = 23
 GPIO.setup(led, GPIO.OUT)
 
+print('Lopp start')
 # Loop 10 times
 for i in range(1, 10):
     GPIO.output(led, 1)
@@ -14,4 +20,7 @@ for i in range(1, 10):
     GPIO.output(led, 0)
     time.sleep(1)
 
+print('Loop over')
 GPIO.cleanup()
+
+print('close')
